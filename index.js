2142dc9d40519ecb7884efea37e636d9fedc1bf8
mathewalexKerala <mathewalexmasai@gmail.com>
2024-08-18 19:06:45 +0530

    dsfjsdkfj

--- index.js
+++ index.js
@@ -14,13 +14,32 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-app.post('/api/shorturl',function(req,res){
+app.post("/api/shorturl", function (req, res) {
+  const emailValid = true;
+  const originalUrl = req.body.url;
+  if (emailValid) {
+    urlArr.push(originalUrl);
+    res.status(200).json({
+      original_url: originalUrl,
+      short_url: urlArr.length,
+    });
+  } else {
+    res.status(400).json({
+      error: "invalid url",
+    });
+  }
+});
 
-  res.json({original_url:`${req.protocol}://${req.get('host')}`,short_url:1})
-})
-app.get('/api/shorturl/:short_url',function(req,res){
-  res.redirect(`${req.protocol}://${req.get('host')}`)
-})
+app.get("/api/shorturl/:short_url", function (req, res) {
+  const short_url = req.params.short_url - 1;
+  const original_url = urlArr[short_url];
+  if (short_url == undefined || original_url < short_url) {
+    res.status(400).json({ respond: "Bad Request" });
+  } else {
+    res.redirect(original_url);
+  }
+});
+
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
