b729972ff5220924ccda50e46f1ed33aba81544d
mathewalexKerala <mathewalexmasai@gmail.com>
2024-08-18 18:39:17 +0530

    commited

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const bodyParser = require('body-parser')
 // Basic Configuration
+app.use(express.json())
 const port = process.env.PORT || 3000;
-
+app.use(bodyParser.urlencoded({extended:false}))
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -13,10 +14,14 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
+app.post('/api/shorturl',function(req,res){
+  console.log(`${req.protocol}://${req.get('host')}`)
+  res.redirect(`${req.protocol}://${req.get('host')}`)
+})
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+  
 });
 
 app.listen(port, function() {
