ff0724a6dd14bb165c49897d6e48d379f97fe359
mathewalexKerala <mathewalexmasai@gmail.com>
2024-08-18 19:09:22 +0530

    dsjfkse32i223

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
+const urlArr = [];
+
 // Basic Configuration
-app.use(express.json())
 const port = process.env.PORT || 3000;
-app.use(bodyParser.urlencoded({extended:false}))
+
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// const isValidUrl = (urlString) => {
+//   var urlPattern = new RegExp(
+//     "^(https?:\\/\\/)?" + //validate protocol
+//       "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + //validate domain name
+//       "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip(v4)
+//       "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + //validate port and path
+//       "(\\?[;&a-z\\d%_.~+=-]*)?" + //validate query string
+//       "(\\#[-a-z\\d_]*)?$",
+//     "i"
+//   ); //validate fragment locator
+//   return !!urlPattern.test(urlString);
+// };
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
+
+// Your first API endpoint
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
 app.post("/api/shorturl", function (req, res) {
   const emailValid = true;
   const originalUrl = req.body.url;
@@ -40,12 +63,6 @@ app.get("/api/shorturl/:short_url", function (req, res) {
   }
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-  
-});
-
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
